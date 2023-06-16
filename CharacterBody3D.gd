a69e4d80776e7134708d6c989bdc11ac8b9f8b10
Saadthedrip <saad.laghrida@gmail.com>
2023-06-16 21:50:01 +0200

    Initial commit

---
+++ CharacterBody3D.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$virid6/AnimationPlayer.play("RUN")
+
+
+
+func _physics_process(delta):
+	
+	
+	
+	var direction = Vector3.ZERO
+	direction.z -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.z += 0.5
+
+
+
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
